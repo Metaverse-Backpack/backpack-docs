56a13a6df681c09f9731a607db0e3e3cc65838fe
Tobias Petrasch <tobiaspetrasch@googlemail.com>
2022-11-30 11:48:00 +0100

    remove deprecated arguments from pandoc

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 rm -rf public
 mkdir public
-pandoc --standalone --from=markdown --to=html --template=template/template.html --output=public/index.html --table-of-contents --css=template/template.css --toc-depth=4 --self-contained --resource-path=. index.md
+pandoc --standalone --from=markdown --to=html --template=template/template.html --output=public/index.html --table-of-contents --css=template/template.css --toc-depth=4 --embed-resources --resource-path=. index.md
